7583870a09515be97b15d39b7126fe0a4a3d9726
kristinakostiuk <krictinamalinovska2003@gmail.com>
2024-08-18 21:44:44 +0300

    fix...

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
-  server: {
-    open: true, 
-  },
+  
 })
