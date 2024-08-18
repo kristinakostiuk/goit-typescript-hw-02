a1e013acdbcdcb16724111014748b1593f1b931c
kristinakostiuk <krictinamalinovska2003@gmail.com>
2024-08-18 21:55:22 +0300

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc'; 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
-})
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    port: 3000, 
+  },
+  resolve: {
+    alias: {
+      '@': '/src', 
+    },
+  },
+});
